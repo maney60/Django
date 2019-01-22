41e226675f8980f0b80877cccd845a6ae5b098f1
maney60 <sudinmaney@gmail.com>
2019-01-22 13:42:57 +0545

    Url mapping and Rendering Blogs

--- Blogs/models.py
+++ Blogs/models.py
@@ -13,3 +13,7 @@ class Post(models.Model):
     def Publish(self):
         self.published_date=timezone.now()
         self.save()
+
+    def __str__(self):
+        return self.title
+
--- Blogs/views.py
+++ Blogs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    return render(request,'Blogs/post_list.html',{})
+
+
+def index(request):
+    posts=Post.objects.all()
+    context={'posts': posts}
+    return render(request,"Blogs/post_list.html",context)
+
